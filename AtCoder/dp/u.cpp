384b72cef8007b915b4ab7d449b505bf92f1b838
tabuchi0919 <tabuchi.igo.0919@gmail.com>
2021-01-04 22:07:44 +0900

    20210104

---
+++ AtCoder/dp/u.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+template <class T> inline bool chmin(T &a, T b) {
+  if(a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <class T> inline bool chmax(T &a, T b) {
+  if(a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+const int INF = (1 << 30) - 1;
+const ll INFLL = (1LL << 61) - 1;
+const int MOD = 1000000007;
+#define ALL(a) (a).begin(), (a).end()
+#define rALL(a) (a).rbegin(), (a).rend()
+#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int N;
+  cin >> N;
+  vector<vector<ll>> A(N, vector<ll>(N));
+  REP(i, N) REP(j, N) cin >> A[i][j];
+  vector<ll> pre(1 << N), dp(1 << N);
+  REP(mask, 1 << N) {
+    REP(i, N) {
+      REP(j, i) {
+        if((mask & (1 << i)) && (mask & (1 << j))) {
+          pre[mask] += A[i][j];
+        }
+      }
+    }
+  }
+  REP(mask, 1 << N) {
+    for(int mask2 = mask; mask2 > 0; mask2 = (mask2 - 1) & mask) {
+      ll nxt = dp[mask - mask2] + pre[mask2];
+      chmax(dp[mask], nxt);
+    }
+  }
+  cout << dp[(1 << N) - 1] << endl;
+}
