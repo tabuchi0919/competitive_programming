48dcc9c97ec8e33210df966de57ef0e25bf0a1e6
tabuchi0919 <tabuchi.igo.0919@gmail.com>
2021-01-22 10:39:31 +0900

    20210122

---
+++ AtCoder/abc040/c.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+template <class T> inline bool chmin(T &a, T b) {
+  if(a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <class T> inline bool chmax(T &a, T b) {
+  if(a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+const int INF = (1 << 30) - 1;
+const ll INFLL = (1LL << 61) - 1;
+const int MOD = 1000000007;
+#define ALL(a) (a).begin(), (a).end()
+#define rALL(a) (a).rbegin(), (a).rend()
+#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int N;
+  cin >> N;
+  vector<ll> a(N), dp(N, INFLL);
+  dp[0] = 0;
+  REP(i, N) cin >> a[i];
+  REP(i, N - 1) {
+    chmin(dp[i + 1], dp[i] + abs(a[i] - a[i + 1]));
+    if(i > 0)
+      chmin(dp[i + 1], dp[i - 1] + abs(a[i - 1] - a[i + 1]));
+  }
+  cout << dp[N - 1] << endl;
+}
